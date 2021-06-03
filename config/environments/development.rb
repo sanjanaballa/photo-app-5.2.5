4f3c093e8c9be2695990fd79fee19713ece845d2
sanjanaballa <sanjana.balla@gmail.com>
2021-06-03 13:49:44 -0700

    setup email confirmation + some layout changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -33,6 +33,17 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => Rails.application.credentials[:sendgrid][:user_name],
+    :password => Rails.application.credentials[:sendgrid][:password],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
 
   config.action_mailer.perform_caching = false
 
